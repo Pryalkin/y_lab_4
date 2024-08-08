04b008128ea2a5917b79e0b184dcbbc956dd0043
pryalkinpp <pppryalkin@gmail.com>
2024-08-08 08:53:41 +0300

    First commit for task2

--- src/main/java/com/pryalkin/Main.java
+++ src/main/java/com/pryalkin/Main.java
@@ -2,6 +2,6 @@ package com.pryalkin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello TASK_2!");
     }
 }
